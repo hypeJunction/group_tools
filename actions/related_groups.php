28971a67365f8ab35e6e08572733003211e4e014
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-02-09 19:26:59 +0100

    Rewrite related groups action

--- actions/related_groups.php
+++ actions/related_groups.php
@@ -1,54 +1,61 @@
 <?php
+
 /**
  * Action to save a new related group
  */
+$group_guid = (int) get_input('group_guid');
+$guid = get_input('guid');
 
-$group_guid = (int) get_input("group_guid");
-$guid = (int) get_input("guid");
+if (empty($group_guid) || empty($guid)) {
+	register_error(elgg_echo('InvalidParameterException:MissingParameter'));
+	forward(REFERER);
+}
 
-if (!empty($group_guid) && !empty($guid)) {
-	$group = get_entity($group_guid);
-	$related = get_entity($guid);
-	
-	// do we have groups
-	if (!empty($group) && elgg_instanceof($group, "group") && !empty($related) && elgg_instanceof($related, "group")) {
-		if ($group->canEdit()) {
-			if ($group->getGUID() != $related->getGUID()) {
-				// not already related?
-				if (!check_entity_relationship($group->getGUID(), "related_group", $related->getGUID())) {
-					
-					if (add_entity_relationship($group->getGUID(), "related_group", $related->getGUID())) {
-						// notify the other owner about this
-						if ($group->getOwnerGUID() != $related->getOwnerGUID()) {
-							$subject = elgg_echo("group_tools:related_groups:notify:owner:subject");
-							$message = elgg_echo("group_tools:related_groups:notify:owner:message", array(
-								$related->getOwnerEntity()->name,
-								elgg_get_logged_in_user_entity()->name,
-								$related->name,
-								$group->name
-							));
-							
-							notify_user($related->getOwnerGUID(), $group->getObjectOwnerGUID(), $subject, $message);
-						}
-						
-						system_message(elgg_echo("group_tools:action:related_groups:success"));
-					} else {
-						register_error(elgg_echo("group_tools:action:related_groups:error:add"));
-					}
-				} else {
-					register_error(elgg_echo("group_tools:action:related_groups:error:already"));
-				}
-			} else {
-				register_error(elgg_echo("group_tools:action:related_groups:error:same"));
-			}
-		} else {
-			register_error(elgg_echo("groups:cantedit"));
-		}
-	} else {
-		register_error(elgg_echo("groups:notfound:details"));
+
+$guid = (is_array($guid)) ? $guid[0] : $guid;
+
+$group = get_entity($group_guid);
+$related = get_entity($guid);
+
+// do we have groups
+if (!elgg_instanceof($group, 'group') || !elgg_instanceof($related, 'group')) {
+	register_error(elgg_echo('groups:notfound:details'));
+	forward(REFERER);
+}
+
+if (!$group->canEdit()) {
+	register_error(elgg_echo('groups:cantedit'));
+	forward(REFERER);
+}
+
+if ($group->guid == $related->guid) {
+	register_error(elgg_echo('group_tools:action:related_groups:error:same'));
+	forward(REFERER);
+}
+
+// not already related?
+if (check_entity_relationship($group->guid, 'related_group', $related->guid)) {
+	register_error(elgg_echo('group_tools:action:related_groups:error:already'));
+	foward(REFERER);
+}
+
+if (add_entity_relationship($group->guid, 'related_group', $related->guid)) {
+	// notify the other owner about this
+	if ($group->owner_guid != $related->owner_guid) {
+		$subject = elgg_echo('group_tools:related_groups:notify:owner:subject');
+		$message = elgg_echo('group_tools:related_groups:notify:owner:message', array(
+			$related->getOwnerEntity()->name,
+			elgg_get_logged_in_user_entity()->name,
+			$related->name,
+			$group->name
+		));
+
+		notify_user($related->owner_guid, $group->owner_guid, $subject, $message);
 	}
+
+	system_message(elgg_echo('group_tools:action:related_groups:success'));
 } else {
-	register_error(elgg_echo("InvalidParameterException:MissingParameter"));
+	register_error(elgg_echo('group_tools:action:related_groups:error:add'));
 }
 
 forward(REFERER);
